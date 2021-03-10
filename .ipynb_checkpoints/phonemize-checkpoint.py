f4684c2d92f2728362f97ebe04c1ec20da82d545
Nicolas DUFOUR <nicolas.dufourn@gmail.com>
2021-03-10 18:04:03 +0100

    Added phonemizer and began data retrieval

--- .ipynb_checkpoints/phonemize-checkpoint.py
+++ .ipynb_checkpoints/phonemize-checkpoint.py
@@ -1,13 +1,27 @@
 import pandas as pd
 from phonemizer import phonemize
  
-# U need to have installed sudo apt-get install festival espeak-ng mbrola
+# You need to have installed sudo apt-get install festival espeak-ng mbrola
 
-def phonemize_df(file_name, column_name):
+def phonemize_labels(file_name, column_name, language):
+    """
+    Phonemize function:
+    This function allow to convert text to phonemes. You need to be sure to have installed the backend beforehand (sudo apt-get install festival espeak-ng mbrola).
+    Parameters:
+    -----------
+        file_name: str
+            Name of the tsv file that contains the sentences we need to phonemize
+        column_name: str
+            Name of the columns that contains the sentences we want to phonemize
+        language: str
+            Language of the sentences. See https://github.com/espeak-ng/espeak-ng/blob/master/docs/languages.md for reference
+        
+    """
+    
     data = pd.read_csv(file_name,sep='\t')
     data['sentence_phonemes'] = phonemize(
         data['sentence'],
-        language='fr-fr',
+        language=language,
         backend = 'espeak',
         njobs = 8
     )
