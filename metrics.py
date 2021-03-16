b6e77d3949d16919d20a6ef4fee9b79acb92336a
Nicolas DUFOUR <nicolas.dufourn@gmail.com>
2021-03-16 16:12:35 +0000

    Added logging and checkpointing

--- metrics.py
+++ metrics.py
@@ -1,14 +1,17 @@
-from pytorch_lightning.metrics import Metrics
+from pytorch_lightning.metrics import Metric
 import editdistance
+import torch
 
-class PER(Metrics):
+class PER(Metric):
     def __init__(self):
+        super().__init__()
         self.add_state("edit_distance", default=torch.tensor(0), dist_reduce_fx="sum")
         self.add_state("target_length", default=torch.tensor(0), dist_reduce_fx="sum")
     def update(self, preds, targets):
-        for i in range(preds.shape[0]):
-            ref_list, pred_list = targets.tolist().remove(0), preds.tolist().remove(0)
-            self.edit_distance += editdistance(ref_list, pred_list)
+        for i in range(len(preds)):
+            ref_list, pred_list = list(filter(lambda a: a != 0, targets.tolist())), preds
+            ref_list, pred_list = list(map(str, ref_list)), list(map(str, pred_list))
+            self.edit_distance += editdistance.distance(ref_list, pred_list)
             self.target_length += len(ref_list)
     def compute(self):
         return self.edit_distance.float() / self.target_length if self.target_length > 0 else 0
