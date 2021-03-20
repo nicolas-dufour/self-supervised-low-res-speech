61ffab438a3cb7b992d0b1539af10080329754c0
Nicolas DUFOUR <nicolas.dufourn@gmail.com>
2021-03-20 13:17:29 +0000

    Fixed PER

--- metrics.py
+++ metrics.py
@@ -9,9 +9,8 @@ class PER(Metric):
         self.add_state("target_length", default=torch.tensor(0), dist_reduce_fx="sum")
     def update(self, preds, targets):
         for i in range(len(preds)):
-            ref_list, pred_list = list(filter(lambda a: a != 0, targets.tolist())), preds
-            ref_list, pred_list = list(map(str, ref_list)), list(map(str, pred_list))
-            self.edit_distance += editdistance.distance(ref_list, pred_list)
-            self.target_length += len(ref_list)
+            targets_list, pred_list = list(filter(lambda a: a != 0, targets[i].tolist())), preds[i]
+            self.edit_distance += editdistance.distance(targets_list, pred_list)
+            self.target_length += len(targets_list)
     def compute(self):
         return self.edit_distance.float() / self.target_length if self.target_length > 0 else 0
